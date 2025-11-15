e86ee2abe432e24adb6c121d1db026bd60b7d6cf
Sk Afroz <dasr43450@gmail.com>
2025-11-15 21:51:13 +0530

    Enhance SEO with title and meta tags
    Added meta tags for SEO and improved page title.

--- app/page.js
+++ app/page.js
@@ -30,6 +30,16 @@ export default function Home() {
   if (!session) {
     return (
       <>
+           <title>Kathabolo App - Chat, Talk & Connect Easily</title>
+      <meta
+        name="description"
+        content="Kathabolo is a smart chatting and talking app that helps you connect instantly. Fast, secure, simple. Try Kathabolo today!"
+      />
+      <meta
+        name="keywords"
+        content="kathabolo, chat app, talking app, messaging app, kathabolo beta, kathabolo netlify"
+      />
+      <meta name="author" content="Kathabolo Team" />
         <div>
           <h1
             onClick={() => {
