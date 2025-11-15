ad45cb0ce34a761e5a7ed4073f93433d45532505
Sk Afroz <dasr43450@gmail.com>
2025-11-15 21:52:05 +0530

    Add meta tags for SEO in layout.js
    Added meta tags for description, keywords, and author.

--- app/layout.js
+++ app/layout.js
@@ -28,6 +28,16 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {/* ✅ Correct SessionProvider wrapping */}
+  <title>Kathabolo App - Chat, Talk & Connect Easily</title>
+      <meta
+        name="description"
+        content="Kathabolo is a smart chatting and talking app that helps you connect instantly. Fast, secure, simple. Try Kathabolo today!"
+      />
+      <meta
+        name="keywords"
+        content="kathabolo, chat app, talking app, messaging app, kathabolo beta, kathabolo netlify"
+      />
+      <meta name="author" content="Kathabolo Team" />
         <SessionProvider>
           <div className="h-[7vh]"> <Navbar /></div>
          
